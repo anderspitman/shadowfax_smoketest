4049ef5f75cf6195564916f23a89ef2ecf401b3a
Anders Pitman <&ers@apitman.com>
2026-07-01 17:17:21 -0600

    Download files in init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+curl -LOR https://github.com/anderspitman/shadowfax_smoketest/releases/download/1.0.0/HG002_chr22.bam
+curl -LOR https://github.com/anderspitman/shadowfax_smoketest/releases/download/1.0.0/HG002_chr22.bam.bai
+
 python3 -m venv pyenv
 source pyenv/bin/activate
 pip install shadowfax
