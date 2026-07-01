7ded6e0746820dbec5cae0a90a5b5a4e0dcc783f
Anders Pitman <&ers@apitman.com>
2026-07-01 17:17:44 -0600

    Add variable for pileup_gbs

--- cuda_test.py
+++ cuda_test.py
@@ -2,7 +2,9 @@
 
 import shadowfax
 
-stream = shadowfax.PileupStream('HG002_chr22.bam')
+PILEUP_GBS = 1
+
+stream = shadowfax.PileupStream('HG002_chr22.bam', output_batch_size=PILEUP_GBS*1024*1024*1024)
 
 for batch in stream:
     print(batch)
