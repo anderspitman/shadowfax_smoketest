212fd2bf58116e169e981774b340f7614b410477
Anders Pitman <&ers@apitman.com>
2026-07-01 17:25:57 -0600

    Print regions instead of objects

--- cuda_test.py
+++ cuda_test.py
@@ -7,4 +7,4 @@ PILEUP_GBS = 1
 stream = shadowfax.PileupStream('HG002_chr22.bam', output_batch_size=PILEUP_GBS*1024*1024*1024)
 
 for batch in stream:
-    print(batch)
+    print(f'{batch.ref_id}:{batch.start_pos}-{batch.end_pos}')
