4d661001b963054b2a9d14fb84a54e601f48ad64
Anders Pitman <&ers@apitman.com>
2026-07-02 17:45:41 -0600

    Fix links

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-curl -LOR https://github.com/anderspitman/shadowfax_smoketest/releases/download/1.0.0/HG002_chr22.bam
-curl -LOR https://github.com/anderspitman/shadowfax_smoketest/releases/download/1.0.0/HG002_chr22.bam.bai
+curl -LOR https://github.com/anderspitman/shadowfax_smoketest/releases/latest/download/HG002_chr22.bam
+curl -LOR https://github.com/anderspitman/shadowfax_smoketest/releases/latest/download/HG002_chr22.bam.bai
 
 python3 -m venv pyenv
 source pyenv/bin/activate
